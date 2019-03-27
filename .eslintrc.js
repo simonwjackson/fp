62f4afff31b593a93108a2f3e544484cb86d78d6
Simon W. Jackson <simon@simonwjackson.com>
2019-03-27 12:10:20 -0500

    first

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "eslint-config-simonwjackson",
+  parserOptions: {
+    sourceType: "module"
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
+  },
+}
